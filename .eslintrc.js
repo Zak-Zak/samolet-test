9322ea92da11239838e5fa8407d4190ed511587b
Гайнуллин Закван Вячеславович <ZGaynullin@alfabank.ru>
2020-11-29 00:12:03 +0500

    chore(*): add eslint, husky and lint-staged

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  extends: ["react-app", "prettier", "prettier/react"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["prettier", "prettier", "react", "@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "error",
+    "no-console": "error",
+    "react/jsx-filename-extension": ["error", { extensions: [".jsx", ".tsx"] }],
+    "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0 }],
+    "no-mixed-spaces-and-tabs": "error",
+    "react/jsx-indent": ["warn", 4, { checkAttributes: true, indentLogicalExpressions: true }],
+  },
+};
