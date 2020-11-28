7b803186cee7aeda504434d90f74f3c9e3e15c7b
Гайнуллин Закван Вячеславович <ZGaynullin@alfabank.ru>
2020-11-29 00:26:42 +0500

    feat(*): new file structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ConfigProvider } from 'antd';
-import { BrowserRouter } from 'react-router-dom';
-import ruRU from 'antd/lib/locale-provider/ru_RU';
-import App from './app';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ConfigProvider } from "antd";
+import { BrowserRouter } from "react-router-dom";
+import ruRU from "antd/lib/locale-provider/ru_RU";
 
-import 'antd/dist/antd.css';
+import App from "./components/app/app";
+
+import "antd/dist/antd.css";
 
 ReactDOM.render(
-  <ConfigProvider locale={ruRU}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ConfigProvider>,
-  document.getElementById('root'),
+    <ConfigProvider locale={ruRU}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </ConfigProvider>,
+    document.getElementById("root")
 );
