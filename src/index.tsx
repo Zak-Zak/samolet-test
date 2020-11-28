7045f36c9f088a254afa174d4bdf4baa439b67e2
Гайнуллин Закван Вячеславович <ZGaynullin@alfabank.ru>
2020-11-28 16:25:54 +0500

    feat(src): replace .js files with .ts/.tsx files

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,8 @@ import { ConfigProvider } from 'antd';
 import { BrowserRouter } from 'react-router-dom';
 import ruRU from 'antd/lib/locale-provider/ru_RU';
 import App from './app';
-import * as serviceWorker from './serviceWorker';
-import 'antd/dist/antd.css';
 
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <ConfigProvider locale={ruRU}>
@@ -16,8 +15,3 @@ ReactDOM.render(
   </ConfigProvider>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
