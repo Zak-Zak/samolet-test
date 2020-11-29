a729bd92039a94b38894763bbf9ac12a9ad36486
Гайнуллин Закван Вячеславович <ZGaynullin@alfabank.ru>
2020-11-29 20:19:57 +0500

    feat(pages): use redux state to get regions information

--- src/pages/root-page/index.tsx
+++ src/pages/root-page/index.tsx
@@ -1,18 +1,37 @@
-import React, { useEffect, useState } from "react";
-import { Layout } from "antd";
+import React, { useEffect } from "react";
+import { Layout, Spin } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { getData } from "../../utils/api";
+import { regionsSelector } from '../../selectors/regions';
+import { regionsActions } from '../../actions/regions';
+import { STATUS } from '../../types/status';
+import { ErrorPage } from '../error-page';
 
 export const RootPage: React.FC = () => {
-    const [data, setData] = useState([]);
+    const dispatch = useDispatch();
+    const { requestStatus, requestError, regions } = useSelector(regionsSelector);
 
     useEffect(() => {
-        getData().then(setData);
-    }, []);
+        dispatch(regionsActions.request());
+    }, [dispatch]);
+
+    let content: JSX.Element;
+
+    switch (requestStatus) {
+        case STATUS.NONE:
+        case STATUS.PENDING:
+            content = (<Spin />);
+            break;
+        case STATUS.FAILURE:
+            content = (<ErrorPage error={ requestError }/>);
+            break;
+        case STATUS.SUCCESS:
+            content = (<pre>{JSON.stringify(regions, null, 2)}</pre>);
+    }
 
     return (
         <Layout>
-            <pre>{JSON.stringify(data, null, 2)}</pre>
+            { content }
         </Layout>
     );
 };
---
+++ src/selectors/regions.ts
@@ -0,0 +1,3 @@
+import { ApplicationState } from '../reducers';
+
+export const regionsSelector = (state: ApplicationState) => state.regions;
